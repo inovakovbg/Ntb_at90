ba9aef6901bfb748a13e02c5730bcee4caaadd84
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-15 17:25:38 +0200

    Another Fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,38 +3,40 @@ import Logo from '../logo/Logo';
 import styles from "./Footer.module.scss";
 import classNames from "classnames";
 import Container from "@mui/material/Container";
-import { Grid, Button, TextField, InputAdornment } from "@mui/material";
+import { Grid, Button, TextField, InputAdornment, Typography } from "@mui/material";
 
 
 export default function Header() {
 
     return (
-      
-            <Container className={classNames(styles.container)} maxWidth="xl">
-                <Grid container spacing={3}
-                    // justifyContent='space-between'
-                    alignItems='center'
-                >
 
-                    <Grid item xs={'1'}>
-                        <Logo type="muted" />
-                    </Grid>
+        <Container className={classNames(styles.container)} maxWidth="xl">
+            <Grid container spacing={3}
+                // justifyContent='space-between'
+                alignItems='center'
+            >
 
-                    <Grid item xs={'7'}>
-                        <p className={styles.paragraph} >Bum All Rights Reserved 2023</p>
-                    </Grid>
+                <Grid item xs={'auto'}>
+                    <Logo type="muted" />
+                </Grid>
 
-                    <Grid item xs={'4'}>
-                        <Button variant="text">Privacy Policy</Button>
-                        <Button variant="text">Coockie Policy</Button>
-                        
+                <Grid item xs={'auto'}>
+                    <Typography className={styles.paragraph}>
+                        Bum All Rights Reserved 2023
+                    </Typography>
+                </Grid>
 
-                    </Grid>
+                <Grid item xs={'auto'}>
+                    <Button variant="text">Privacy Policy</Button>
+                    <Button variant="text">Coockie Policy</Button>
+
 
                 </Grid>
 
-            </Container>
-        
+            </Grid>
+
+        </Container>
+
 
     )
 }
