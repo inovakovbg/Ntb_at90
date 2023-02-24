80304ae2a7e49c77d45ef628210a78b26bcafe10
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-24 14:51:51 +0200

    Start ProductInfoLikes

--- pages/index.jsx
+++ pages/index.jsx
@@ -28,6 +28,7 @@ import ProductImage from "../src/components/product/ProductImage";
 import ProductInfoTitle from "../src/components/product/ProductInfoTitle";
 import ProductInfoPrice from "../src/components/product/ProductInfoPrice";
 import ProductInfoStatus from "../src/components/product/ProductInfoStatus";
+import ProductInfoLikes from "../src/components/product/ProductInfoLikes";
 
 
 
@@ -354,6 +355,7 @@ export default function Index() {
          <ProductInfoTitle text="Shallow Son" />
          <ProductInfoPrice amount={3} currency="ETH"/>
          <ProductInfoStatus/>
+         <ProductInfoLikes amount={5400}/>
 
          <br></br>
          <Footer />
---
+++ src/components/product/ProductInfoLikes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./ProductInfoLikes.module.scss";
+import { Chip } from "@mui/material";
+import CircleIcon from "@mui/icons-material/Circle";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import millify from "millify";
+
+export default function ProductInfoLikes({amount=0}) {
+  return (
+    <div className={styles["product-info-status"]}>
+      <Chip className={styles.likes} color="secondary" icon={<FavoriteIcon />} label={millify(amount)} variant="outlined" />
+    </div>
+  )
+
+}
