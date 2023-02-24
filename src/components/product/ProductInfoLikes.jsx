705df2c4d246fb4e4ad7a079d8bb3e29db058571
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-24 14:57:31 +0200

    Change root class

--- src/components/product/ProductInfoLikes.jsx
+++ src/components/product/ProductInfoLikes.jsx
@@ -7,7 +7,7 @@ import millify from "millify";
 
 export default function ProductInfoLikes({amount=0}) {
   return (
-    <div className={styles["product-info-status"]}>
+    <div className={styles["product-info-likes"]}>
       <Chip className={styles.likes} color="secondary" icon={<FavoriteIcon />} label={millify(amount)} variant="outlined" />
     </div>
   )
