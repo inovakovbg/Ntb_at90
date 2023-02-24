7cb58bacceaba148fe591d2399c39b6575704624
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-24 16:18:07 +0200

    Change paper to div

--- src/components/product/ProductInfoCreator.jsx
+++ src/components/product/ProductInfoCreator.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import styles from "./ProductInfoCreator.module.scss";
 import Typography from '@mui/material/Typography';
-import Container  from "@mui/material/Container";
+import Container from "@mui/material/Container";
 import User from "../user/User";
 import classNames from "classnames";
 import { Paper } from "@mui/material";
@@ -9,27 +9,19 @@ import { Paper } from "@mui/material";
 
 
 
-export default function ProductInfoCreator({name, avatar, verified=false}) {
+export default function ProductInfoCreator({ name, avatar, verified = false }) {
 
 
 
   return (
-    <Paper className={styles["product-info-creator"]}>
+    <div className={styles["product-info-creator"]}>
       <Typography className={styles.title}>CREATOR</Typography>
 
       <Container container className={styles["user-container"]} disableGutters>
-     
-          
-         
-
-            <User size='53px' name={name}  verified={verified} avatar={avatar}/>
-
-
-     
-
+        <User size='53px' name={name} verified={verified} avatar={avatar} />
       </Container>
 
-      </Paper>
+    </div>
 
   )
 }
