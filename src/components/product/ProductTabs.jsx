f6c5f63cc2cb7cc36614e135ba31e141210b5510
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-26 19:52:03 +0200

    Start ProductTabs

---
+++ src/components/product/ProductTabs.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+
+import styles from './ProductTabs.module.scss';
+import { Stack, Grid } from '@mui/material';
+
+export default function ProductTabs({ text,bids = [] }) {
+  
+
+  return (
+    <div className={styles['product-tabs']}>
+      
+
+
+    </div>
+  );
+}
+
+
+
+
