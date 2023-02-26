5dfc835aeffc19b64beb7c5ea8532e9c79643fc7
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-26 12:09:16 +0200

    Add precise

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -8,7 +8,7 @@ import ProductInfoCreator from './ProductInfoCreator';
 import ProductInfoTimer from './ProductInfoTimer';
 import ProductInfoStatus from './ProductInfoStatus';
 import styles from './ProductInfo.module.scss';
-import { Stack,Grid } from '@mui/material';
+import { Stack, Grid } from '@mui/material';
 
 export default function ProductInfo({ title, creator, price, currency, likes, onTimeEnd, timeEnd, isLive }) {
   const { name, avatar, verified } = creator;
@@ -16,22 +16,30 @@ export default function ProductInfo({ title, creator, price, currency, likes, on
   return (
     <div className={styles['product-info']}>
       <ProductInfoTitle text={title} />
+      
+      <div className={styles["title-line"]}></div>
 
-      <Stack direction="row" spacing={2} className={styles.stats}>
-        <ProductInfoPrice amount={price} currency={currency} />
+      <Stack direction="row" spacing={2} className={styles.bigstack}>
+
+        <Stack direction="row" spacing={2} className={styles.stats}>
+          <ProductInfoPrice amount={price} currency={currency} />
+          {isLive && <ProductInfoStatus />}
+        </Stack>
         <ProductInfoLikes amount={likes} />
+
+      
       </Stack>
 
       <Grid container spacing={2}>
         <Grid item xs={7}>
-          <ProductInfoCreator name='ivan' avatar='images/avatar.png' verified={true} />
+          <ProductInfoCreator name={name} avatar={avatar} verified={verified} />
         </Grid>
         <Grid item xs={5}>
           <ProductInfoTimer timeEnd={timeEnd} onTimeEnd={onTimeEnd} />
         </Grid>
       </Grid>
 
-      {isLive && <ProductInfoStatus />}
+
     </div>
   );
 }
