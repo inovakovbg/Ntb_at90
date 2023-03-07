100ba49d63d58957c76659653ec9d335e308dea1
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-08 01:01:05 +0200

    Probably ready

---
+++ pages/product/[id]/index.jsx
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/router';
+import nfts from '../../../data/nfts.json';
+import ProductContainer from '../../../src/components/product/ProductContainer';
+import Header from '../../../src/components/header/Header';
+import Footer from '../../../src/components/footer/Footer';
+
+export default function Product() {
+  const router = useRouter();
+  const { id } = router.query;
+  const product = nfts.find((nft) => nft.id === parseInt(id));
+
+  return (
+    <div>
+      <Header />
+      <ProductContainer {...product} />
+      <Footer />
+    </div>
+
+
+
+  );
+}
