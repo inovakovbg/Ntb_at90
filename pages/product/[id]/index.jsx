75fcfb75023e328e7746ef38c6984b79460e9b02
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-08 01:09:52 +0200

    TT

--- pages/product/[id]/index.jsx
+++ pages/product/[id]/index.jsx
@@ -5,6 +5,7 @@ import Header from '../../../src/components/header/Header';
 import Footer from '../../../src/components/footer/Footer';
 
 export default function Product() {
+  
   const router = useRouter();
   const { id } = router.query;
   const product = nfts.find((nft) => nft.id === parseInt(id));
