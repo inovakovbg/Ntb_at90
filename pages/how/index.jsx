8eb2ffa0a19d34573f8c7bdf3102f361285a1c56
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-09 19:03:53 +0200

    Start Hero

---
+++ pages/how/index.jsx
@@ -0,0 +1,15 @@
+
+import Header from '../../src/components/header/Header';
+import Footer from '../../src/components/footer/Footer';
+import Hero from '../../src/components/hero/Hero';
+
+export default function How() {
+
+  return (
+    <div>
+      <Header />
+      <Hero text="HOW IT WORKS" />
+      <Footer />
+    </div>
+  );
+}
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./Hero.module.scss";
+import Typography from "@mui/material/Typography";
+import { Container } from '@mui/material';
+
+
+export default function Hero({ text }) {
+
+    return (
+        <div className={styles.hero}>
+        <Container>
+            <Typography variant='h1' className={styles.text}>{text} </Typography>
+      
+            </Container>
+        </div>
+
+    )
+}
+
+
