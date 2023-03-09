82566c6fb18cc417c8b27ee4a780bbf6d901e4d8
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-09 22:45:05 +0200

    Start ProfileUser

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,6 +1,7 @@
 
 import Header from '../../src/components/header/Header';
 import Footer from '../../src/components/footer/Footer';
+import ProfileUser from '../../src/components/profile/ProfileUser';
 import ProfileHero from '../../src/components/profile/ProfileHero';
 
 export default function Profile() {
@@ -9,6 +10,15 @@ export default function Profile() {
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
       <Header />
       <ProfileHero image="/images/avatar.png"/>
+      <ProfileUser 
+      
+        name="fredrika_a"
+        info="What are they they are the broken promise and to do is out of reach.
+        Hand over the candy notes food is so expensive to the Cashier.
+        "
+        avatar="/images/avatar.png"
+        verified={true}    
+      />
       <Footer />
     </div>
   );
---
+++ src/components/profile/ProfileUser.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from "./ProfileUser.module.scss";
+import { Typography } from '@mui/material';
+import Avatar from '../avatar/Avatar';
+
+
+
+export default function ProfileUser({name,info,avatar,verified}) {
+
+    return (
+        <div className={styles['profile-user']}>
+             <Avatar size={192} url={avatar} verified={verified}/>
+             <Typography variant='h3' className={styles.name} >{name} </Typography>
+             <Typography variant='body1'className={styles.info} >{info} </Typography>
+
+        </div>
+    )
+}
+
+
