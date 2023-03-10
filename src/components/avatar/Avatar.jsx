9061c1d401ab9976be290f566a78d2a163d88a2e
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-11 00:47:41 +0200

    change avatar

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -5,7 +5,7 @@ import styles from "./Avatar.module.scss";
 export default function Avatar({size=90 ,url,verified=true}) {
     return( 
     <div className={classNames(styles.avatar)} style={{width:size, height:size}}>
-            <img className={classNames(styles.image)} src="/images/avatar.png"/> 
+            <img className={classNames(styles.image)} src={url}/> 
             {verified && <img className={classNames(styles.badge)}  src={'/images/verified.svg'}/> } 
     </div>            
     )
