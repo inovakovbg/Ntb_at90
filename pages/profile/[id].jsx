19017ff9f4c69651628ddc125533781a8a3c328b
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-10 18:03:51 +0200

    start [id].jsx

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -4,7 +4,6 @@ import Footer from '../../src/components/footer/Footer';
 import ProfileUser from '../../src/components/profile/ProfileUser';
 import ProfileHero from '../../src/components/profile/ProfileHero';
 import filtersData from '../../data/filtersProfile.json';
-import ProfileCollectionFilters from '../../src/components/profile/ProfileCollectionFilters';
 import ProfileCollection from '../../src/components/profile/ProfileCollection';
 import profileData from '../../data/profile.json';
 
@@ -13,18 +12,15 @@ export default function Profile() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
       <Header />
-      <ProfileHero image="/images/avatar.png" />
+      <ProfileHero image={profileData.avatar.backgroundUrl} />
       <ProfileUser
 
         name={profileData.username}
-        info="What are they they are the broken promise and to do is out of reach.
-        Hand over the candy notes food is so expensive to the Cashier.
-        "
-        avatar="/images/avatar.png"
-        verified={true}
+        info={profileData.info}
+       
+        avatar={profileData.avatar.url}
+        verified={profileData.verified}
       />
-
-      {/* <ProfileCollectionFilters filters={filtersData} /> */}
 
       <ProfileCollection user={profileData} filters={filtersData} items={profileData.nfts}/>
 
