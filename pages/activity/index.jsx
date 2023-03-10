768dac1adf1eb13342b7178e894aa72fe070bb5d
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-10 22:45:03 +0200

    Probably ready

--- pages/activity/index.jsx
+++ pages/activity/index.jsx
@@ -3,19 +3,24 @@ import Header from '../../src/components/header/Header';
 import Footer from '../../src/components/footer/Footer';
 import Hero from '../../src/components/hero/Hero';
 
+import activity from '../../data/activity.json';
+
 
 import filtersData from '../../data/filtersActivity.json';
 import ActivityFilters from '../../src/components/activity/ActivityFilters';
 import ActivityListItem from '../../src/components/activity/ActivityListItem';
 
-export default function How() {
+export default function Activity() {
+
+  const act=activity[0];
+  console.log({act})
 
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
       <Header />
       <Hero text='Activity'/>
       <ActivityFilters filters={filtersData} />
-      <ActivityListItem/>
+      <ActivityListItem user={act.user.username} nft={act.nft} created_at={act.nft.created_at}/>
       <Footer />
     </div>
   );
--- src/components/activity/ActivityListItem.jsx
+++ src/components/activity/ActivityListItem.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Avatar from "../avatar/Avatar";
 import styles from "./ActivityListItem.module.scss"
+import Link from "../link/Link";
+import formatDistance from 'date-fns/formatDistance';
+import parseISO from 'date-fns/parseISO';
 
-export default function ActivityListItem({ user, created_at, nft, type }) {
+
+export default function ActivityListItem({ user, created_at, nft, type = 'liked' }) {
 
 
 
     return (
         <div className={styles['activity-list-item']}>
-            <Avatar  size={90} url='images/avatar.png'  />
+            <Avatar size={90} url='images/avatar.png' />
             <div className={styles['activity-list-item-details']}>
-            <p>1233
-            </p>
-      <p>12331</p>
+                <p>
+                    {user} {type} 
+                    <Link href={`/product/${nft.id}`}>
+                        "{nft.name}"
+                    </Link> by    
+                    <Link href={`/profile/${nft.owner.username}`}>
+                        {nft.owner.username}
+                    </Link>
+                </p>
+                
+                <p>{formatDistance(parseISO(created_at), new Date())}</p>
             </div>
         </div>
-    );
+    )
 };
-
 
