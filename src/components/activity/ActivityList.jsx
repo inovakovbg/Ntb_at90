87aaef8975b54b8f4d68e0fa7001ad30599948ba
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-11 12:44:24 +0200

    add key index

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -16,8 +16,8 @@ export default function ActivityList({ items }) {
     return (
         <div className={classNames(styles["activity-list"])}>
             <Stack direction="column" spacing={2}>
-                {items.map((item) => {
-                    return <ActivityListItem key={item.index} user={item.user} created_at={item.created_at} nft={item.nft} type={item.type}
+                {items.map((item,index) => {
+                    return <ActivityListItem key={index} user={item.user} created_at={item.created_at} nft={item.nft} type={item.type}
                     />
                 })}
             </Stack>
