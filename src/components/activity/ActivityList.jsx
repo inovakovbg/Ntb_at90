52339850e94af50c3c215fecc3de5e500374afb4
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-11 18:10:52 +0200

    Cont

--- src/components/activity/ActivityList.jsx
+++ src/components/activity/ActivityList.jsx
@@ -4,20 +4,21 @@ import styles from "./ActivityList.module.scss"
 import Link from "../link/Link";
 import formatDistance from 'date-fns/formatDistance';
 import parseISO from 'date-fns/parseISO';
-import Stack from "@mui/material/Stack";
+import {Container, Stack} from "@mui/material";
 import { useState, useEffect, useRef } from "react";
 import ActivityListItem from "./ActivityListItem";
 import classNames from "classnames";
 
 
-export default function ActivityList({items = []}) {
+export default function ActivityList({ items }) {
+
     return (
-        <div className={classNames(styles["activity-list"])}>
-             <Stack direction="column" spacing={2}>
-                {items.map((item, index) => {
-                    return <ActivityListItem key={index} user={item.user} created_at={item.created_at} nft={item.nft} type={item.type}  />
-                })}
-             </Stack>
-        </div>
-    )
+        <Container maxWidth='xl' className={styles['activity-list']}>
+            <Stack  direction="column" spacing={2} >
+                {items.map((item, i) => (
+                    <ActivityListItem key={i} user={item.user} created_at={item.created_at} nft={item.nft} type={item.type} />
+                ))}
+            </Stack>
+        </Container>
+    );
 }
