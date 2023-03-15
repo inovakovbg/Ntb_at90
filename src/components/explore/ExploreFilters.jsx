4ca8e930f3445d99903343c44d8d2966349d19ac
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-15 10:11:56 +0200

    Center

--- src/components/explore/ExploreFilters.jsx
+++ src/components/explore/ExploreFilters.jsx
@@ -1,24 +1,11 @@
 import { FormControl, InputLabel, MenuItem, Select, Stack, TextField, InputAdornment } from '@mui/material';
-import { Search } from '@mui/icons-material';
 import styles from './ExploreFilters.module.scss';
 import SearchIcon from "@mui/icons-material/Search";
-import { useState } from 'react';
+
 
 
 export default function ExploreFilters({ filters }) {
     const { sort, price } = filters;
-
-    // const [sortBy, setSortBy] = useState('');
-    // const [priceRange, setPriceRange] = useState('');
-
-    // const handleSortByChange = (event) => {
-    //   setSortBy(event.target.value);
-    // };
-
-    // const handlePriceRangeChange = (event) => {
-    //   setPriceRange(event.target.value);
-    // };
-
 
     return (
         <div className={styles['explore-filters']}>
@@ -54,9 +41,9 @@ export default function ExploreFilters({ filters }) {
 
 
 
-                <TextField className={styles.textField} type="search"
+                <TextField  className={styles.textField} type="search"
                     variant="standard"
-                    // placeholder="Search NFTs"
+                    placeholder="Search NFTs"
                    
                     InputProps={{
                         startAdornment: (
