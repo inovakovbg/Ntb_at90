3e7bd0ef722db18da96af9144fd58ba32ef77aae
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-16 19:22:23 +0200

    Start Impl.Profile Page API Calls

--- pages/profile/[id].jsx
+++ pages/profile/[id].jsx
@@ -1,4 +1,6 @@
 
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import Header from '../../src/components/header/Header';
 import Footer from '../../src/components/footer/Footer';
 import ProfileUser from '../../src/components/profile/ProfileUser';
@@ -8,21 +10,44 @@ import ProfileCollection from '../../src/components/profile/ProfileCollection';
 import user from '../../data/profile.json';
 
 export default function Profile() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [profile, setProfile] = useState(null);
+  const [profileFilters, setProfileFilters] = useState(null);
+
+  useEffect(async () => {
+    try {
+      console.log(`/users/${id}`);
+      const result = await fetch(process.env.apiUrl + `/users/${id}`);
+      if (!result.ok) {
+        throw new Error(`HTTP error! status: ${result.status}`);
+      }
+      const profileData = await result.json();
+      setProfile(profileData.user);
+      setProfileFilters(profileData.fiters);
+    } catch (error) {
+      console.error('Error fetching profile data:', error);
+      console.error('Server response:', await result.text());
+    }
+  }, [id]);
+
+
+
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
       <Header />
-      <ProfileHero image={user.avatar.backgroundUrl} />
+      <ProfileHero image={profile.avatar.backgroundUrl} />
       <ProfileUser
 
-        name={user.username}
-        info={user.info}
+        name={profile.username}
+        info={profile.info}
        
-        avatar={user.avatar.url}
-        verified={user.verified}
+        avatar={profile.avatar.url}
+        verified={profile.verified}
       />
 
-      <ProfileCollection user={user} filters={filtersData} items={user.nfts}/>
+      <ProfileCollection user={profile} filters={filtersData} items={profile.nfts}/>
 
       <Footer />
     </div>
