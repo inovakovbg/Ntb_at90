d9665f0a649260ca66a8811e0d6249378c75c9d5
Ivan Novakov <inovakov.bg@gmail.com>
2023-03-16 11:16:44 +0200

    Start Explore page

--- pages/explore/index.jsx
+++ pages/explore/index.jsx
@@ -4,16 +4,45 @@ import Footer from '../../src/components/footer/Footer';
 import ExploreTitle from '../../src/components/explore/ExploreTitle';
 import filtersData from '../../data/filtersExplore.json';
 import ExploreFilters from '../../src/components/explore/ExploreFilters';
+import { Container, Grid } from '@mui/material';
+import nfts from '../../data/nfts.json';
+import Card from '../../src/components/card/Card';
 
 export default function Explore() {
 
 
 
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+    <div >
       <Header />
-       <ExploreTitle text='Explore'/>
-       <ExploreFilters filters={filtersData} />
+      <Container maxWidth='xl' disableGutters>
+
+        <Grid container spacing={2} sx={{ margin: '10px 0' }} justifyContent="space-between" alignItems="center">
+          <Grid item xs={5}>
+            <ExploreTitle text='Explore' />
+          </Grid>
+          <Grid item xs={7}>
+            <ExploreFilters filters={filtersData} />
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={1} sx={{ margin: '10px 0' }} justifyContent="space-between" >
+          {nfts.map((card) => {
+
+            return (
+              <Grid Grid item xs={3}>
+                <Card name={card.name}
+                  user={card.owner}
+                  likes={card.likes}
+                  mediaUrl={card.mediaUrl}
+                  price={card.price}
+                  currency={card.currency}
+                />
+              </Grid>)
+          })}
+        </Grid>
+
+      </Container>
       <Footer />
     </div>
   );
